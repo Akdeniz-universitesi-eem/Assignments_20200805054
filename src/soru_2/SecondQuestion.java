0688cf185ae4236b0a4fa7a20be61ed98adbeace
Sinan Demir <sinanmaraz4@gmail.com>
2023-10-24 12:44:50 +0300

    update Question2

--- src/soru_2/SecondQuestion.java
+++ src/soru_2/SecondQuestion.java
@@ -19,8 +19,8 @@ public class SecondQuestion {
         JFrame frame = new JFrame("1. ZAR");
         JFrame frame1 = new JFrame("2. ZAR");
         // Resmi yükler
-        ImageIcon dice1 = new  ImageIcon("C:\\Users\\dell\\Desktop\\zar\\"+zar1+".jpeg");
-        ImageIcon dice2 = new ImageIcon("C:\\Users\\dell\\Desktop\\zar\\"+zar2+".jpeg");
+        ImageIcon dice1 = new  ImageIcon("C:\\Users\\dell\\IdeaProjects\\Main\\src\\soru_2\\Dicepictures\\"+zar1+".jpeg");
+        ImageIcon dice2 = new ImageIcon("C:\\Users\\dell\\IdeaProjects\\Main\\src\\soru_2\\Dicepictures\\"+zar2+".jpeg");
         // label oluşturur ve resmi içine atar
         JLabel label = new JLabel(dice2);
         JLabel label1 = new JLabel(dice1);
